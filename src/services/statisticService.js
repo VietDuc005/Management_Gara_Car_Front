562f89a750de1a7a55a8bfd60acf81819fb690a5
VietDuc005 <ducsmile111@gmail.com>
2025-10-25 21:30:45 +0700

    update: thong ke phieu sua chua

--- src/services/statisticService.js
+++ src/services/statisticService.js
@@ -40,4 +40,8 @@ export const statisticService = {
   async getTopDichVu() {
     return await apiCall(`/api/thongke/top-5-dich-vu`, { method: "GET" });
   },
+  //dùng cho phiếu sửa chữa
+  async getThongKePhieu() {
+    return await apiCall(`/api/thongke/phieu-sua-chua`, { method: "GET" });
+  },
 };
